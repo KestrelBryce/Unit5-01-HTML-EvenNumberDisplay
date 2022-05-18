ad9eb0e4927ca9cc000f5f11ab5f77756cec3be7
Kestrel.Bryce <99274358+KestrelBryce@users.noreply.github.com>
2022-05-18 14:47:53 +0000

    Finished?

---
+++ js/script.js
@@ -0,0 +1,31 @@
+  /* This function gets the user's Dairy Queen order and calculates the cost. 
+*/
+
+function displayGreeting () {
+
+    // user input
+  let minNum = parseInt((document.getElementById("min-Number-Here").value));
+    
+  let maxNum = parseInt((document.getElementById("max-Number-Here").value));
+
+
+    // initializing counter
+  let counter = minNum
+
+
+    // initializing numbers
+  let numbers = ""
+
+  
+    // loop code
+  while  (counter <= maxNum)  {
+    // building string of numbers with line break
+    numbers = numbers + counter + "<br>"
+    // add to the counter
+    counter = counter + 1
+  }
+
+  // return string
+  document.getElementById("greeting").innerHTML = numbers
+    
+}
